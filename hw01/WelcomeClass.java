b4937d641ea6985fd9980ee20d0f97ea253c8b69
mdiaz31 <mmd318@lehigh.edu>
2014-09-16 17:51:55 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+//define a class
+public class WelcomeClass {
+
+    //add a main method
+    public static void main(String[] args) {
+
+
+        //print the text
+        
+        System.out.println("   -----------");
+        System.out.println("   | Welcome |");
+        System.out.println("   -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-M--M--D--3--1--8-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("My name is Michael and I play club Rugby and club Ice Hockey"); 
+        System.out.println("here at Lehigh.");
+        
+        
+    }
+        
+        
+}
